ab631ab5fa1db49c1df79215aaade8c2d947c2b6
Vishnu5L3 <142379885+Vishnu5L3@users.noreply.github.com>
2024-02-05 00:16:20 -0600

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,10 +1,12 @@
 '''
-Camera Classifier v0.1 Alpha
-Copyright (c) NeuralNine
-
-Instagram: @neuralnine
-YouTube: NeuralNine
-Website: www.neuralnine.com
+This is a Camera Classifier Code.
+We can use this for Image Classifier and almost the same code to identify celestial objects as well .We only need to update model.py:
+By Adjusting preprocessing and feature extraction techniques.
+By Training the classifier with appropriate model and dataset.
+camera.py:
+By Loading satellite images instead of camera frames.
+app.py:
+Update GUI elements for star identification context.
 '''
 
 import cv2 as cv
